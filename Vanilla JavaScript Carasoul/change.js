feb01dc5c98419279b6710b44351de761b05c3bb
Braden Niksich <43426747+abniksi@users.noreply.github.com>
2019-03-03 20:20:05 -0600

    Add files via upload
    Not the prettiest carousel, but was more focused on the JavaScript and getting it to function like a carousel. May update later with smooth transitions and a better design.

---
+++ Vanilla JavaScript Carasoul/change.js
@@ -0,0 +1,20 @@
+let pictures = document.querySelector('#chicago');
+let next = document.querySelector('#next');
+let previous = document.querySelector('#previous');
+let incrementCounter = 1;
+
+next.addEventListener("click", () =>{
+  ++incrementCounter;
+  if (incrementCounter === 4) {
+    incrementCounter = 1;
+  }
+  pictures.src = 'images/chicago' + incrementCounter + '.jpg'
+});
+
+previous.addEventListener("click", () =>{
+  --incrementCounter;
+  if(incrementCounter === 0){
+    incrementCounter = 3;
+  }
+  pictures.src = 'images/chicago' + incrementCounter + '.jpg'
+});
