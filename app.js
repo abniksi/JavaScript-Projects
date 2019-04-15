040b5356046a8f658a9cf3c0d536620c00282076
Braden Niksich <43426747+abniksi@users.noreply.github.com>
2019-04-15 10:36:18 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+
+//"/" => "Hi there!"
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+
+// "/bye" = > "Goodbye!"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+})
+
+// "/dog" => "MEOW!"
+app.get("/dog", function(req, res){
+    res.send("MEOW!!!!")
+});
+
+app.get("/r/:subredditName", function(req, res) {
+   var subreddit = req.params.subredditName;
+   res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("*", function(req, res) {
+    res.send("YOU ARE A STAR!");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");
+});
